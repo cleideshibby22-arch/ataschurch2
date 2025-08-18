570b810f549c70e08ff05e9a7741f9e206a71496
paulobastosbarbosa <168569265+paulobastosbarbosa@users.noreply.github.com>
2025-08-18 09:30:33 -0300

    Corrigir estrutura do App.tsx com AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -16,31 +16,37 @@ import ProprietarioPanel from './pages/ProprietarioPanel'
 import { AtaProvider } from './context/AtaContext'
 import { AuthProvider } from './context/AuthContext'
 
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/proprietario-login" element={<ProprietarioLogin />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/esqueci-senha" element={<EsqueciSenha />} />
+      <Route path="/proprietario" element={<ProprietarioPanel />} />
+      <Route path="/*" element={
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/nova-ata" element={<NovaAta />} />
+            <Route path="/editar-ata/:id" element={<NovaAta />} />
+            <Route path="/atas" element={<ListaAtas />} />
+            <Route path="/ata/:id" element={<VisualizarAta />} />
+            <Route path="/usuarios" element={<Usuarios />} />
+            <Route path="/oracoes-sacramentais" element={<OracoesSacramentais />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </Layout>
+      } />
+    </Routes>
+  )
+}
+
 function App() {
   return (
     <AuthProvider>
       <AtaProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/proprietario-login" element={<ProprietarioLogin />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-          <Route path="/proprietario" element={<ProprietarioPanel />} />
-          <Route path="/*" element={
-            <Layout>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/nova-ata" element={<NovaAta />} />
-                <Route path="/editar-ata/:id" element={<NovaAta />} />
-                <Route path="/atas" element={<ListaAtas />} />
-                <Route path="/ata/:id" element={<VisualizarAta />} />
-                <Route path="/usuarios" element={<Usuarios />} />
-                <Route path="/oracoes-sacramentais" element={<OracoesSacramentais />} />
-                <Route path="/admin" element={<Admin />} />
-              </Routes>
-            </Layout>
-          } />
-        </Routes>
+        <AppRoutes />
       </AtaProvider>
     </AuthProvider>
   )
